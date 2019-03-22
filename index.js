55d9ce900569a2ab4f7b69b5b512d6188164e846
Ay Has <ahashim983@yahoo.com>
2019-03-22 00:45:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function viewCart(){
   } else {
     var cart_format = []
     for (var i = 0; i < cart.length; i++){
-      cart_format.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`)
+      cart_format.push(`${cart[i].itemName} at $${cart[i].itemPrice} `)
     }
     cart_format[cart_format.length-1] = `and ${cart_format[cart_format.length-1]}`
     return `In your cart, you have ${cart_format.join('')}`
