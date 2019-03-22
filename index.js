ca0ec45ddbed6fa2a53edee5f138370e1eafc492
Ay Has <ahashim983@yahoo.com>
2019-03-22 01:35:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ function removeFromCart(item){
   for (var i = 0; i < cart.length; i++){
     if (cart[i].itemName == item){
       delete cart[i].itemName
+      delete cart[i].itemPrice
       j = j+2
     }
   }
