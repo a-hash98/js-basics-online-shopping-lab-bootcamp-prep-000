3739debd5547ece810ce087075f55a989d45b2bd
Ay Has <ahashim983@yahoo.com>
2019-03-22 02:18:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,8 +46,15 @@ function removeFromCart(item){
   for (var i = 0; i < cart.length; i++){
     if (cart[i].itemName == item){
       cart.splice(i,1)
+      j = j + 1
+      break
     }
   }
+  if (j==0){
+    return "That item is not in your cart."
+    
+  }
+ 
 }
 
 
